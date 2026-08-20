2f874bbb59b0f3b9c5e82431f3e1124990d4d6a2
FocusFlow Team <team@focusflow.cto>
2026-08-20 01:00:41 +0000

    Professional redesign of portfolio: modern palette, fixed typos, real project cards, SEO/OG meta, correct nav/contact links

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Gamer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Front-End Developer", "Web Designer", "Self-Taught Coder", "Lifelong Learner", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Gamer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Front-End Developer", "Web Designer", "Self-Taught Coder", "Lifelong Learner", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
