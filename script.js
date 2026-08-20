b932a92e35962da7495dcfa50db12c2a3bdb7817
FocusFlow Team <team@focusflow.cto>
2026-08-20 01:40:30 +0000

    Rebrand to full-stack developer; fix nav CTA spacing; remove Web Calculator card

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-End Developer", "Web Developer", "React Developer", "Website Designer", "Freelancer"],
+        strings: ["Full-Stack Developer", "Web Developer", "React Developer", "Website Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
